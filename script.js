2f043c1bc52b7af5b08c250a5dbc11577a7da423
wahyu12v <mkzcmd@gmail.com>
2024-03-05 11:06:18 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function () {
+    document.querySelectorAll('nav a').forEach(anchor => {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault();
+
+            document.querySelector(this.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
+    });
+});
+
+// berfungsi untuk memberikan animasi dropdown saat menekan menu di navbar
